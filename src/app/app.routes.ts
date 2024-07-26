11827fc79bf8a54e55a6fad91f3cc7b3c0366e8b
Edwin Restrepo <egrestrepa@eafit.edu.co>
2024-07-26 10:27:04 -0500

    Actualizar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: "inicio", component: InicioComponent},
+    {path: "nosotros", component: NosotrosComponent},
+    {path: "tienda", component: TiendaComponent},
+
+];
